7a5d137abc13c70f240ef78a01423248c5cc40f0
Frédéric NOËL <contact@fredericnoel.com>
2022-06-07 16:39:06 +0200

    Appel fonction includeIncFiles()

--- includes/main.php
+++ includes/main.php
@@ -3,14 +3,6 @@
 ?>
 <main>
 <?php
-    $files = glob('./includes/*.inc.php');
-    $page = isset($_GET['page']) ? $_GET['page'] : "accueil";
-    $page = "./includes/" . $page . ".inc.php";
-
-    if (in_array($page, $files)) 
-        require $page;
-    else 
-        require './includes/accueil.inc.php';
-
+    inclusionIncFile('contact');
 ?>
 </main>
