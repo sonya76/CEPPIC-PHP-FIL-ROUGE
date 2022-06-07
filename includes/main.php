227bb2709af777dc6d90bd832ce0938e18f2760d
Frédéric NOËL <contact@fredericnoel.com>
2022-06-07 15:07:19 +0200

    Récupération superglobale

--- includes/main.php
+++ includes/main.php
@@ -2,5 +2,15 @@
     require_once './includes/nav.php';
 ?>
 <main>
+<?php
+    $page = isset($_GET['page']) ? $_GET['page'] : "N'existe pas";
 
+    /*if (isset($_GET['page']))
+        $page = $_GET['page'];
+
+    else
+        $page = "N'existe pas";
+    */
+    echo $page;
+?>
 </main>
