52a8649bb195057834e6dfceca62d3103f3f1f9a
Frédéric NOËL <contact@fredericnoel.com>
2022-06-08 09:18:47 +0200

    Ajout d'éléments dans le menu

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,10 @@
 <header>
     <nav>
         <ul>
-            <li><a href="index.php">Accueil</a></li>
+            <li><a href="index.php?page=accueil">Accueil</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+            <li><a href="index.php?page=inscription">Inscription</a></li>
+            <li><a href="index.php?page=login">Login</a></li>
         </ul>
     </nav>
 </header>
