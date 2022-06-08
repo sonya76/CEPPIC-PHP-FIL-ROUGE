aef637625b3e09eb73dea1b68494578bc86862ed
Frédéric NOËL <contact@fredericnoel.com>
2022-06-08 09:52:49 +0200

    Ajout formulaire inscription

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -1,1 +1,19 @@
 <h1>Inscription</h1>
+<form action="index.php?page=inscription" method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" />
+    </div>
+    <div>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" />
+    </div>
+    <div>
+        <label for="mail">e-mail :</label>
+        <input type="text" id="mail" name="mail" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+</form>
