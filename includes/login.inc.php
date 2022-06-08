489fc893d099badf021ba10ce1a522e6d4557656
Frédéric NOËL <contact@fredericnoel.com>
2022-06-08 15:21:59 +0200

    Ajout formulaire login et contrôles

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,39 @@
 <h1>Login</h1>
+<?php
+    if (isset($_POST['frmLogin'])) {
+        $mail = htmlentities(trim($_POST['mail']));
+        $mdp = htmlentities(trim($_POST['mdp']));
+
+        $erreurs = array();
+
+        if (mb_strlen($mail) === 0)
+            array_push($erreurs, "Il manque votre e-mail");
+
+        if (mb_strlen($mdp) === 0)
+            array_push($erreurs, "Il manque votre mot de passe");
+
+        if (count($erreurs)) {
+            $messageErreur = "<ul>";
+
+            for($i = 0 ; $i < count($erreurs) ; $i++) {
+                $messageErreur .= "<li>";
+                $messageErreur .= $erreurs[$i];
+                $messageErreur .= "</li>";
+            }
+    
+            $messageErreur .= "</ul>";
+    
+            echo $messageErreur;
+
+            include './includes/frmLogin.php';
+        }
+
+        else {
+            include './includes/frmLogin.php';
+        }
+    }
+    
+    else {
+        $nom = $prenom = $mail = "";
+        include './includes/frmLogin.php';
+    }
