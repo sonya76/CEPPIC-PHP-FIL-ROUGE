93f1cd1d7205e3cadc9a4fac219ee561bda3e8f0
Frédéric NOËL <contact@fredericnoel.com>
2022-06-08 10:13:52 +0200

    Suppression de la partie HTML

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -1,28 +1,12 @@
 <h1>Inscription</h1>
 <?php
-    if (isset($_POST['frmInscription']))
+    if (isset($_POST['frmInscription'])) {
         $message = "Je viens du formulaire";
+        dump($_POST);
+    }
+    
     else
         $message = "Je ne viens pas du formulaire";
 
     echo $message;
 ?>
-<form action="index.php?page=inscription" method="post">
-    <div>
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" />
-    </div>
-    <div>
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" />
-    </div>
-    <div>
-        <label for="mail">e-mail :</label>
-        <input type="text" id="mail" name="mail" />
-    </div>
-    <div>
-        <input type="reset" value="Effacer" />
-        <input type="submit" value="Envoyer" />
-    </div>
-    <input type="hidden" name="frmInscription" />
-</form>
