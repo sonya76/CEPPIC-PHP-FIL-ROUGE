af5495f56df42293199b7235bf1446741604518c
Frédéric NOËL <contact@fredericnoel.com>
2022-06-08 11:14:28 +0200

    Modifications mineures

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-require_once './functions/autoLoadFunction.php';
-
 date_default_timezone_set('Europe/Paris');
 
+require_once './functions/autoLoadFunction.php';
 
 require_once './includes/head.php';
 require_once './includes/main.php';
