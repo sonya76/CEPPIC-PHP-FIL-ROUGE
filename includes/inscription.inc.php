a2d685c616edd10bcce8013c8aa9118ac9113929
Frédéric NOËL <contact@fredericnoel.com>
2022-06-08 15:03:55 +0200

    Vérification erreurs, affichage

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -2,11 +2,44 @@
 <?php
     if (isset($_POST['frmInscription'])) {
         $message = "Je viens du formulaire";
-        dump($_POST);
+        
+        $nom = htmlentities(trim($_POST['nom']));
+        $prenom = htmlentities(trim($_POST['prenom']));
+        $mail = htmlentities(trim($_POST['mail']));
+
+        $erreurs = array();
+
+        if (mb_strlen($nom) === 0)
+            array_push($erreurs, "Il manque votre nom");
+
+        if (mb_strlen($prenom) === 0)
+            array_push($erreurs, "Il manque votre prénom");
+
+        if (mb_strlen($mail) === 0)
+            array_push($erreurs, "Il manque votre e-mail");
+
+        if (count($erreurs)) {
+            $messageErreur = "<ul>";
+
+            for($i = 0 ; $i < count($erreurs) ; $i++) {
+                $messageErreur .= "<li>";
+                $messageErreur .= $erreurs[$i];
+                $messageErreur .= "</li>";
+            }
+    
+            $messageErreur .= "</ul>";
+    
+            echo $messageErreur;
+
+            include './includes/frmInscription.php';
+        }
+
+        else {
+            displayMessage("Pas d'erreurs");
+        }
     }
     
-    else
-        $message = "Je ne viens pas du formulaire";
-
-    echo $message;
-?>
+    else {
+        $nom = $prenom = $mail = "";
+        include './includes/frmInscription.php';
+    }
