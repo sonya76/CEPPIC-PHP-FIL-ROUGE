e2850aeaf9d9544816cf1610eeb246119bb728dd
Frédéric NOËL <contact@fredericnoel.com>
2022-06-21 11:45:12 +0200

    Modifications mineures (suppression commentaire, réagencement)

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
+
 session_start();
+
+date_default_timezone_set('Europe/Paris');
 
 spl_autoload_register(function ($className) {
     require './classes/' . $className . '.php';
 });
 
-date_default_timezone_set('Europe/Paris');
-
 require_once './functions/autoLoadFunction.php';
-
 require_once './includes/head.php';
 require_once './includes/main.php';
 require_once './includes/footer.php';
-
-// $_SESSION['count'] = ($_SESSION['count'] ?? 0) + 1;
-
-// dump($_SERVER);
-// dump($_SERVER['REMOTE_ADDR']);
-// dump($_SERVER,'var_dump');
