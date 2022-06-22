21ed98634d345bffb39c3745e7b004bf4b7f0765
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-06-22 12:40:15 +0200

    ajout interfaceUtilisateur

---
+++ classes/InterfaceUtilisateur.php
@@ -0,0 +1,10 @@
+<?php
+
+interface InterfaceUtilisateur
+{
+    public function inscrireUtilisateur();
+    public function modifierUtilisateur();
+    public function supprimerUtilisateur();
+    public function connecterUtilisateur();
+    public function deconnecterUtilisateur();
+}
