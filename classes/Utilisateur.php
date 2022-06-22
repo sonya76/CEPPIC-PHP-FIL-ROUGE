b9f363e6d2d16cd3719b4f7d68199ae1c46d0c65
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-06-22 11:29:33 +0200

    ajout class utilisateur

---
+++ classes/Utilisateur.php
@@ -0,0 +1,30 @@
+<?php
+
+class Utilisateur implements Utilisateur
+{
+    public function inscrireUtilisateur()
+    {
+        
+    }
+
+    public function modifierUtilisateur()
+    {
+        
+    }
+
+    public function supprimerUtilisateur()
+    {
+        
+    }
+
+    public function connecterUtilisateur()
+    {
+        
+    }
+
+    public function deconnecterUtilisateur()
+    {
+        
+    }
+
+}
